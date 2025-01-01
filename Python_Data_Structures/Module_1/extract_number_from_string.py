272820833586e1f2a56ecee9f20f7832a824750b
JustinWermske <justin.d.wermske@gmail.com>
2025-01-01 15:48:29 -0600

    Added comments

--- Python_Data_Structures/Module_1/extract_number_from_string.py
+++ Python_Data_Structures/Module_1/extract_number_from_string.py
@@ -1,6 +1,9 @@
 """
 6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
 """
+# 1. find the position of the colon
+# 2. remove whitespace
+# 3. convert to float
 
 text = "X-DSPAM-Confidence:    0.8475"
 pos = text.find(":")
